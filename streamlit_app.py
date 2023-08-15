2cde0e298f10bfa68411da407b8551b16b5cc0cc
Sreten <sreten.milivoevich@gmail.com>
2023-08-15 16:17:28 +0200

    Update streamlit_app.py
    sinhronizovano sa originalom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Manu')
-streamlit.text('Omega 3 & Blueberry')
-streamlit.text('Kale, Spinach')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
