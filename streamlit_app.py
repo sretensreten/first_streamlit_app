62c26594e8f747e690e5f48d0aa28041d3da4a27
Sreten <sreten.milivoevich@gmail.com>
2023-08-15 16:16:03 +0200

    Update streamlit_app.py
    ispravka sintaksne greske

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-stremlit.header('Breakfast Manu')
+streamlit.header('Breakfast Manu')
 streamlit.text('Omega 3 & Blueberry')
 streamlit.text('Kale, Spinach')
