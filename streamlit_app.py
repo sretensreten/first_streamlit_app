5fa679737d12c9c182a8931469beab7418a3d3a7
Sreten <sreten.milivoevich@gmail.com>
2023-08-24 13:39:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,31 @@ streamlit.dataframe(fruit_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+      streamlit.error("Please select fruit to get info.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  sreamlit.error()
+
 streamlit.write('The user entered ', fruit_choice)
 
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 # import snowflake.connector
 
