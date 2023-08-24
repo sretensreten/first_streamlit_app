3eec10baa7833f4c4499381c5fd93901e58feba2
Sreten <sreten.milivoevich@gmail.com>
2023-08-24 13:56:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,17 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+         return my_cur.fetchall()
+# add the button to load the fruit
+def streamlit.button('Get Fruit Load List') 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
 #don't run anything past here while we troubleshoot
 # streamlit.stop()
 
@@ -60,9 +71,9 @@ streamlit.write('The user entered ', fruit_choice)
 
 # import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
